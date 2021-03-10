081025846795824fb862e499c655ba5a26b2993f
Grant McCarty <46609447+GrantMcCarty@users.noreply.github.com>
2021-03-10 10:21:54 -0600

    Fixed jetpack-component

--- Doug-Digs/games/doug-digs/components/jetpack-component.js
+++ Doug-Digs/games/doug-digs/components/jetpack-component.js
@@ -6,18 +6,33 @@ export default  class JetpackComponent extends Engine.Component {
     this.speed = speed;
     this.fuel = 100;
     this.maxFuel = 100;
+    this.offsetX = 0.25;
+    this.offsetY = 0;
+    this.lastMovPos = "right";
   }
   update() {
+    if(this.gameObject.parent) {
+      if((Engine.Input.getKey("ArrowLeft") || Engine.Input.getKey('a'))) {
+        this.lastMovPos = "left";
+      } else if((Engine.Input.getKey("ArrowRight") || Engine.Input.getKey('d'))) {
+        this.lastMovPos = "right";
+      }
+      this.gameObject.transform.x = this.lastMovPos === "left" ? 
+        this.gameObject.parent.x + this.offsetX : this.gameObject.parent.x - this.offsetX;
+      this.gameObject.transform.y = this.gameObject.parent.y + this.offsetY;
+    }
     if ((Engine.Input.getKey("ArrowUp") || Engine.Input.getKey('w')) && this.fuel > 0) {
-        this.fuel -= this.speed * 0.5;
-        // console.log(this.fuel);
+      this.gameObject.parent.y -= 0.10;
+      this.fuel -= this.speed * 0.5;
+      this.offsetY = -0.1;
+    } else {
+      this.offsetY = 0;
     }
-    if(Engine.Input.getKey(" ")) {
-        this.fuel = this.maxFuel;
+    if (Engine.Input.getKey(" ")) {
+      this.fuel = this.maxFuel;
     }
   }
-  upgradeFuel(){
-      this.maxFuel += this.maxFuel*0.5;
+  upgradeFuel() {
+    this.maxFuel += this.maxFuel * 0.5;
   }
-
 }
